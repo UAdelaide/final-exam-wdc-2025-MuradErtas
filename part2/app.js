2a851e10301040a06839f1f271e9236a19f20738
MuradErtas <muradbalzan@gmail.com>
2025-06-20 07:05:37 +0000

    [automatic commit] 2025-06-20 07:05:37 UTC 101.115.234.113

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
