5f96725e09e16bc4c411553da773024e5861453d
MuradErtas <muradbalzan@gmail.com>
2025-06-20 00:49:04 +0000

    [automatic commit] 2025-06-20 00:49:03 UTC 101.115.234.113

--- part1/insertStatements.sql
+++ part1/insertStatements.sql
@@ -2,5 +2,5 @@ INSERT INTO Users (username, email, passwword_hash, role) VALUES
 ('alice123','alice@example.com','hashed123','owner'),
 ('bobwalker','bob@example.com','hashed456','walker'),
 ('carol123','carol@example.com''hashed789','owner'),
-('frankwalker','dave@example.com','hashed101','walker'),
-('eve123','eve@example.com','hashed'
+('frankwalker','frank@example.com','hashed100','walker'),
+('eve123','eve@example.com','hashed101','owner')
