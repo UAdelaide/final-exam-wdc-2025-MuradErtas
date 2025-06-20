c68245fd12d63477df62af894851db6f8901ed52
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:08:33 +0000

    [automatic commit] 2025-06-20 02:08:32 UTC 101.115.234.113

--- part1/app.js
+++ part1/app.js
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use
+app.use('/api', require('./routes/api'));
 
 let db;
 
