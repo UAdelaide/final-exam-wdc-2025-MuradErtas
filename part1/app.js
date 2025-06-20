818b09874e277311408933096f5bfc56786aa2b2
MuradErtas <muradbalzan@gmail.com>
2025-06-20 03:04:25 +0000

    [automatic commit] 2025-06-20 03:04:25 UTC 101.115.234.113

--- part1/app.js
+++ part1/app.js
@@ -133,7 +133,7 @@ let db;
 
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-        (1, )
+        (1, 2, 1, 5, 'Great walk!'),
       `);
     }
   } catch (err) {
