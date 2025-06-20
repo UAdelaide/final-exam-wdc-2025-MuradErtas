d931c4d1fc1b4fd020bfc55588de4194a064eb5d
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:39:08 +0000

    [automatic commit] 2025-06-20 02:39:07 UTC 101.115.234.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,8 @@ const db = require('../db');
 // GET /api/dogs
 router.get('/dogs', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT name, size, onwer FROM Dogs');
+        const [rows] = await db.query('
+            SELECT name FROM Dogs');
         res.json(rows);
     } catch (err) {
         console.error('Error fetching dogs:', err);
