6f2548bae50fdc7b3e40f6d5767addadc982dbe8
MuradErtas <muradbalzan@gmail.com>
2025-06-20 06:54:44 +0000

    [automatic commit] 2025-06-20 06:54:44 UTC 101.115.234.113

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -55,6 +55,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.clear
+    res.json({ message: 'Logged out successfully' });
+  });
+});
 
 module.exports = router;
