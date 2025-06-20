5cba083e6cc08790bf89c83ef4df62a2c3181922
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:59:33 +0000

    [automatic commit] 2025-06-20 02:59:33 UTC 101.115.234.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -43,6 +43,7 @@ router.get('/walkers/summary', async (req, res) => {
             AVG(WalkRatings.rating) AS average_rating,
             COUNT(DISTINCT WalkRequests.request_id) AS completed_walks
             FROM Users
+            LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
         `);
         res.json(rows);
     } catch (err) {
