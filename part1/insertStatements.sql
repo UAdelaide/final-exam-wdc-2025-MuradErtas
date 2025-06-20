7492bfce817184f516b1507f52208ea224e9c048
MuradErtas <muradbalzan@gmail.com>
2025-06-20 00:54:10 +0000

    [automatic commit] 2025-06-20 00:54:10 UTC 101.115.234.113

--- part1/insertStatements.sql
+++ part1/insertStatements.sql
@@ -14,8 +14,8 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Levi', 'large')
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2023-10-01 10:00:00', 30, 'Park A', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2023-10-02 11:00:00', 45, 'Park B', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2023-10-03 09:00:00', 60, 'Park C', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Lucy'), '2023-10-04 08:00:00', 30, 'Park D', 'open')
-((SELECT dog_id FROM Dogs WHERE name = 'Levi'), '2023-10-04 08:00:00', 30, 'Park D', 'open')
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Park A', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Park B', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-10 09:30:00', 60, 'Park C', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Lucy'), '2025-06-10 09:30:00', 30, 'Park D', 'open')
+((SELECT dog_id FROM Dogs WHERE name = 'Levi'), '2025-06-10 09:30:00', 30, 'Park D', 'open')
