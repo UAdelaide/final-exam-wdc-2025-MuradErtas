73ab7bf75b70309a4ab56cd2ec2ddd1791a27637
MuradErtas <muradbalzan@gmail.com>
2025-06-20 01:58:34 +0000

    [automatic commit] 2025-06-20 01:58:34 UTC 101.115.234.113

--- part1/app.js
+++ part1/app.js
@@ -99,12 +99,12 @@ let db;
     `);
 
     await db.execute(`
-      INSERT INTO Users (user_id, username, email, password_hash, role) VALUES
-      (1, 'alice123', 'alice@example.com', 'hashed123', 'owner'),
-      (2, 'bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-      (3, 'carol123', 'carol@example.com', 'hashed789', 'owner'),
-      (4, 'frankwalker', 'frank@example.com', 'hashed100', 'walker'),
-      (5, 'eve123', 'eve@example.com', 'hashed101', 'owner');
+      INSERT INTO Users (username, email, password_hash, role) VALUES
+      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+      ('frankwalker', 'frank@example.com', 'hashed100', 'walker'),
+      ('eve123', 'eve@example.com', 'hashed101', 'owner');
     `);
 
     await db.execute(`
