54299d17992063972b2fd83aa144254340989374
MuradErtas <muradbalzan@gmail.com>
2025-06-20 09:29:20 +0000

    [automatic commit] 2025-06-20 09:29:20 UTC 101.115.234.113

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -6,9 +6,7 @@ const db = require('../models/db');
 router.get('/', async (req, res) => {
     try {
         const [rows] = await db.query(`
-            SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
-            FROM Dogs
-            JOIN Users ON Dogs.owner_id = Users.user_id
+            SELECT dog_id,
             `);
         res.json(rows);
     } catch (err) {
