f10c35d147fbad70c6c52ebaa0e18f4eb37fc23e
MuradErtas <muradbalzan@gmail.com>
2025-06-20 01:55:37 +0000

    [automatic commit] 2025-06-20 01:55:37 UTC 101.115.234.113

--- part1/app.js
+++ part1/app.js
@@ -99,7 +99,7 @@ let db;
     `);
 
     await db.execute(`
-      INSERT INTO Users (user_id, username, email, password_hash, role) VALUES
+      INSERT INTO IF NOT EXISTS Users (user_id, username, email, password_hash, role) VALUES
       (1, 'alice123', 'alice@example.com', 'hashed123', 'owner'),
       (2, 'bobwalker', 'bob@example.com', 'hashed456', 'walker'),
       (3, 'carol123', 'carol@example.com', 'hashed789', 'owner'),
@@ -108,7 +108,7 @@ let db;
     `);
 
     await db.execute(`
-      INSERT INTO Dogs (owner_id, name, size) VALUES
+      INSERT INTO IF NOT EXISTS Dogs (owner_id, name, size) VALUES
       ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
       ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
       ((SELECT user_id FROM Users WHERE username = 'eve123'), 'Charlie', 'medium'),
