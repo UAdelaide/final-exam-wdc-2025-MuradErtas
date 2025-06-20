0c6685484dc8a011dd9056130d41d265fa388598
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:52:34 +0000

    [automatic commit] 2025-06-20 02:52:33 UTC 101.115.234.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -21,7 +21,7 @@ router.get('/dogs', async (req, res) => {
 router.get('/walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.query(`
-            SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, Users.username AS owner_username, WalkRequests.location
+            SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
             FROM WalkRequests
             JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
             JOIN Users ON Dogs.owner_id = Users.user_id
