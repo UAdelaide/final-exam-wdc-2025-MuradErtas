898054932538426165b193fd9bad134c0d8dd0bd
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:45:11 +0000

    [automatic commit] 2025-06-20 02:45:11 UTC 101.115.234.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -21,7 +21,7 @@ router.get('/dogs', async (req, res) => {
 router.get('/walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.query(`
-            
+            SELECT 
             `);
         res.json(rows);
     } catch (err) {
