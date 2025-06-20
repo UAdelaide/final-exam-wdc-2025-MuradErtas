7da6fcab267fd78e0bf589ab6124276055d13c4f
MuradErtas <muradbalzan@gmail.com>
2025-06-20 00:54:55 +0000

    [automatic commit] 2025-06-20 00:54:55 UTC 101.115.234.113

--- part1/insertStatements.sql
+++ part1/insertStatements.sql
@@ -18,4 +18,4 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Park B', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-07-10 01:30:00', 60, 'Park C', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Lucy'), '2025-09-10 21:30:00', 30, 'Park D', 'open')
-((SELECT dog_id FROM Dogs WHERE name = 'Levi'), '2025-09-10 09:30:00', 30, 'Park D', 'open')
+((SELECT dog_id FROM Dogs WHERE name = 'Levi'), '2025-09-11 12:30:00', 30, 'Park D', 'open')
