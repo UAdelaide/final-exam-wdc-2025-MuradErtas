5b7e049e671539eb0925747668c5bb06838a0c35
MuradErtas <muradbalzan@gmail.com>
2025-06-20 09:28:17 +0000

    [automatic commit] 2025-06-20 09:28:17 UTC 101.115.234.113

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,1 +1,16 @@
 const express
+
+// GET /dogs
+router.get('/', async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+            SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+            FROM Dogs
+            JOIN Users ON Dogs.owner_id = Users.user_id
+            `);
+        res.json(rows);
+    } catch (err) {
+        console.error('Error fetching dogs:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
