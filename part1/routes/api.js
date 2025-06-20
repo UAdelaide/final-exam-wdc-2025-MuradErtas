91972e3837cad4d46401e063f0d5c0ba921dd1b7
MuradErtas <muradbalzan@gmail.com>
2025-06-20 02:03:44 +0000

    [automatic commit] 2025-06-20 02:03:43 UTC 101.115.234.113

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2/promise');
+const app = require('../app');
 
-
+router.get('/api', async (req, res) => {
+  try {
+    // Connect to the database
+    const db = await mysql.createConnection({
+      host: 'localhost',
 
 module.exports = router;
